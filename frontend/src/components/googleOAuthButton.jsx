35e26d2acfff48b6bda7e48899d50740a39f5005
Luka Juha <luka.juha@fer.hr>
2025-11-10 16:15:01 +0100

    Ažurirao README i bazu podataka

--- frontend/src/components/googleOAuthButton.jsx
+++ frontend/src/components/googleOAuthButton.jsx
@@ -9,6 +9,20 @@ export default function GoogleLoginButton({role}) {
           const user = jwtDecode(res.credential);
           console.log("Google user:", user);
           console.log("Role: ", role);
+          /*
+          const response = await fetch("http://localhost:8000/api/auth/google/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            res.credential,
+            role,
+          }),
+        });
+
+        const data = await response.json();
+          */
         }}
         onError={() => console.log("Login Failed")}
       />
