913c53a67a3a410c60285badb1c56e87a0f8eaf8
Luka Juha <luka.juha@fer.hr>
2026-01-23 20:01:20 +0100

    Trying again

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,6 @@ pip install -r requirements.txt
 
 python manage.py makemigrations
 
-python manage.py collectstatic --no-input
+python manage.py migrate --run-syncdb
 
-python manage.py migrate
+python manage.py collectstatic --no-input
